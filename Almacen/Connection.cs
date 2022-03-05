bb40877ae2dd5eb07738b413b317ec2d9bf010f4
KryptomSilver <abelromeror763@gmail.com>
2022-03-04 22:47:22 -0600

    Agregar archivos de proyecto.

---
+++ Almacen/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Npgsql;
+
+namespace Almacen
+{
+    internal class Connection
+    {
+        public void openConnection()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection();
+            string uriConnection;
+            uriConnection = "Host=127.0.0.1;User Id=postgres;Password=1234;Database=almacen";
+            conn.ConnectionString = uriConnection;
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Base de datos conectada");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar la base de datos");
+            }
+        }
+
+    }
+}
