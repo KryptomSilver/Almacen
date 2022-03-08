64d75ceaf715c4c9ba45bb37940842eb67d50552
KryptomSilver <abelromeror763@gmail.com>
2022-03-08 14:43:06 -0600

    Added connection and insert posgresql

--- Almacen/Connection.cs
+++ Almacen/Connection.cs
@@ -10,22 +10,13 @@ namespace Almacen
 {
     internal class Connection
     {
-        public void openConnection()
+        public NpgsqlConnection openConnection()
         {
             NpgsqlConnection conn = new NpgsqlConnection();
             string uriConnection;
             uriConnection = "Host=127.0.0.1;User Id=postgres;Password=1234;Database=almacen";
             conn.ConnectionString = uriConnection;
-            try
-            {
-                conn.Open();
-                MessageBox.Show("Base de datos conectada");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error al conectar la base de datos");
-            }
-        }
-
+            return conn;
+        }        
     }
 }
--- Almacen/MainWindow.xaml.cs
+++ Almacen/MainWindow.xaml.cs
@@ -21,13 +21,52 @@ namespace Almacen
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        Connection server = new Connection();
+        public class Producto
+        {
+            public int clave { get; set; }
+            public string Nombre { get; set; }
+        }
+        public void  getProducts()
+        {
+            NpgsqlConnection conn = server.openConnection();
+            conn.Open();
+            NpgsqlCommand command = new NpgsqlCommand("SELECT * from productos", conn);
+            NpgsqlDataReader dr = command.ExecuteReader();
+            List<Producto> productos = new List<Producto>();
+            while (dr.Read())
+            {
+                productos.Add(new Producto()
+                {
+                    clave = (int)dr[0],
+                    Nombre = (string)dr[1],
+                });
+            }
+            data.ItemsSource = productos;
+        }
         public MainWindow()
         {
             InitializeComponent();
-            Connection conn = new Connection();
-            conn.openConnection();
-        }       
+            getProducts();
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NpgsqlConnection conn = server.openConnection();
+            conn.Open();
+            string commandoSQL;
+            string input = nombreInput.Text;
+            commandoSQL = "INSERT INTO productos (nombre) VALUES('"+ input +" ')";
+            NpgsqlCommand command = new NpgsqlCommand(commandoSQL, conn);
+            int res = command.ExecuteNonQuery();
+            if (res > 0)
+            {
+                getProducts();
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
     }
 }
