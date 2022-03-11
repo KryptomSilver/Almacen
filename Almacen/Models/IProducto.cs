d690bdda5a36411c9df9d89463d610c7a7bb28f0
KryptomSilver <abelromeror763@gmail.com>
2022-03-11 14:46:54 -0600

    Added model product and user

---
+++ Almacen/Models/IProducto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen.Models
+{
+    public interface IProducto
+    {
+       public int clave { get; set; }
+       public string nombre { get; set; }
+    }
+}
---
+++ Almacen/Models/IUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen.Models
+{
+    public interface IUsuario
+    {
+        public string nombre { get; set; }
+        public int codEmpleado { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string tipoUsuario { get; set; }
+    }
+}
