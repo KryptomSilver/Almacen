c7375b0a2019d42f4422dc3752c856a7db2cac7d
KryptomSilver <abelromeror763@gmail.com>
2022-03-11 15:19:40 -0600

    Create view model

---
+++ Almacen/ViewModels/ProductoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Almacen.Models;
+
+namespace Almacen.ViewModels
+{
+    public abstract class ProductoViewModel 
+    {
+      
+    }
+}
